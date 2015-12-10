940694bad4e5ceded88a894dbec209c95ebb2588
unknown <dbridenbecker@NA01678L.na.corp.intven.com>
2015-12-09 16:54:23 -0800

    Added comments

--- componentTests/componentTests.cpp
+++ componentTests/componentTests.cpp
@@ -27,10 +27,13 @@ int main(int argc, char* argv[])
 
     if( argc == 2 )
     {
+        // argv[1] is the name of a suite that you would like to run like AssortivityTest.
+        // It runs all of the tests in that suite.
         return UnitTest::RunSuite( argv[1] );
     }
     else
     {
+        // Run all of the test suites
         return UnitTest::RunAllTests();
     }
 }
