1ae2f09b4f6c8d98c86988c6c74d21ffa860b549
Jonathan Bloedow <jhb4@cornell.edu>
2016-02-10 23:27:52 -0800

    Changed larval habitat input config format (parsing) per 2.5 spec. Need to migrate config.json files over still.

--- Eradication/VectorSpeciesParameters.h
+++ Eradication/VectorSpeciesParameters.h
@@ -18,6 +18,18 @@ To view a copy of this license, visit https://creativecommons.org/licenses/by-nc
 
 namespace Kernel
 {
+    class LarvalHabitatParams : public JsonConfigurable
+    {
+        IMPLEMENT_DEFAULT_REFERENCE_COUNTING()
+        virtual QueryResult QueryInterface(iid_t iid, void **ppvObject) { return e_NOINTERFACE; }
+
+        public:
+            LarvalHabitatParams() {}
+            virtual void ConfigureFromJsonAndKey( const Configuration* inputJson, const std::string& key );
+            virtual json::QuickBuilder GetSchema();
+            std::map< VectorHabitatType::Enum, float > habitat_map; 
+    };
+
     class VectorSpeciesParameters : public JsonConfigurable
     {
         IMPLEMENT_DEFAULT_REFERENCE_COUNTING()
@@ -28,8 +40,7 @@ namespace Kernel
         bool Configure( const ::Configuration *json );
         virtual QueryResult QueryInterface(iid_t iid, void **ppvObject);
 
-        std::vector<float> habitat_param;
-        std::vector<VectorHabitatType::Enum> habitat_type;
+        LarvalHabitatParams habitat_params;
         float aquaticarrhenius1;
         float aquaticarrhenius2;
         float infectedarrhenius1;
