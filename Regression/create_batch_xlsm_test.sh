b6139e94e8397e12ac3f33de7e8701f2f88fb785
Jonathan Bloedow <jbloedow@intven.com>
2016-03-08 12:53:36 -0800

    HIV Scenarios 03 XLSM tested and pass.

---
+++ Regression/create_batch_xlsm_test.sh
@@ -0,0 +1,8 @@
+
+
+for file in `find Scenarios/STIAndHIV/03_Transmission/ -name param_overrides.json|sed 's/param_overrides.json//g'`; do 
+    echo "python y:\Scripts\xslm2json.py " $file/config.xlsm; 
+    echo "python .\extract_overrides.py defaults\hiv_scenario_default_config.json .config_from_excel.json > " $file\param_overrides.json 
+    echo "del .config_from_excel.json "
+done > ./batch_convert_xlsm_to_json.bat
+
