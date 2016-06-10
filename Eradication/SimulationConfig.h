60eda7fe758d43e3f0be286e5f93a05f8df307c8
Daniel Bridenbecker <dbridenbecker@intven.com>
2016-06-09 20:28:28 -0700

    removed some unused member variables from SimulationConfig

--- Eradication/SimulationConfig.h
+++ Eradication/SimulationConfig.h
@@ -281,8 +281,6 @@ namespace Kernel
 #endif
 #ifndef DISABLE_STI
         // STI: these will all move to IndividualHumanSTIConfig soon.
-        float shortTermRelationshipLength;
-        float concurrentRelationshipLength;
         float prob_super_spreader;
         bool  enable_coital_dilution;
 
