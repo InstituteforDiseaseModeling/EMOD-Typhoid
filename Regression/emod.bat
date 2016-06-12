e56106107ad325c870f6d4498630597ede930d25
Daniel Bridenbecker <dbridenbecker@intven.com>
2016-06-12 15:19:55 -0700

    GitHub-506 - Failure running STI/HIV tutorials via Excel front-end. Config.xlsm for STI/HIV tutorials need to be updated

--- Regression/emod.bat
+++ Regression/emod.bat
@@ -1,1 +1,14 @@
-Z:\Eradication.exe -C config.xlsm -O testing -I Z:\Scenarios\InputFiles --python-script-path=Z:\Python
+
+echo Setting PATH=Z:\;Z:\Python27;%PATH%
+set PATH=Z:\;c:\Python27;%PATH%
+
+:: 'Official' Python path environment variable
+set PYTHONPATH=c:\Python27
+
+:: IDM Python path environment variable
+set PYTHON_PATH=c:\Python27
+
+:: https://docs.python.org/2/using/windows.html
+set PYTHONHOME=c:\Python27
+
+Z:\Eradication.exe -C config.xlsm -O testing -I Z:\Scenarios\InputFiles --python-script-path=z:\Python
