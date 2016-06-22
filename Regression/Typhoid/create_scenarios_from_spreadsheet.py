7ff80d7e4189e1e6299d7103949c4326ee54bd19
Jonathan Bloedow <jbloedow@intven.com>
2016-06-22 16:43:20 -0700

    Updated regressions per wiki, from script. Regenerated pngs.

--- Regression/Typhoid/create_scenarios_from_spreadsheet.py
+++ Regression/Typhoid/create_scenarios_from_spreadsheet.py
@@ -2,6 +2,7 @@ import csv
 import sys
 import json
 import os
+import shutil
 
 def num(s):
     try:
@@ -32,6 +33,23 @@ for scenario in params_json.keys():
         po_json["parameters"] = params_json[config_name_id]
         po_json["parameters"]["Default_Config_Path"] = "defaults/typhoid_default_config.json"
         po_json["parameters"]["Config_Name"] = config_name
+        demog_hint = po_json["parameters"].pop( "demographics" )
+        if demog_hint == "large":
+            po_json["parameters"]["Base_Population_Scale_Factor"] = 10
+        elif demog_hint == "small":
+            po_json["parameters"]["Base_Population_Scale_Factor"] = 0.1
+        campaign_hint = po_json["parameters"].pop( "outbreaks" )
+        
+        if campaign_hint == "one large":
+            shutil.copy( "campaign_onelarge_template.json", os.path.join( config_name, "campaign.json" ) )
+            po_json["parameters"]["Simulation_Duration"] = 30
+        elif campaign_hint == "two large":
+            shutil.copy( "campaign_twolarge_template.json", os.path.join( config_name, "campaign.json" ) )
+            po_json["parameters"]["Simulation_Duration"] = 60
+        elif campaign_hint == "annual":
+            shutil.copy( "campaign_annual_template.json", os.path.join( config_name, "campaign.json" ) )
+            po_json["parameters"]["Simulation_Duration"] = 1825
+
         po.write( json.dumps( po_json, sort_keys = True, indent=4 ) )
 
 print( "Done!" )
