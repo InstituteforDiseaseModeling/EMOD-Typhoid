37cb20b67dceb7b1b4f4ea3f6c68cbaf5380e43e
Christopher Lorton <clorton@intven.com>
2016-08-02 15:49:03 -0700

    Fix #350 - Clean Up (Delete?) InputParser Directory

--- InputDataParser/stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// All_Inclusive_Parser.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
