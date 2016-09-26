4aa8d8d0d26845982a73740c24d4419242521de9
Jonathan Bloedow <jhb4@cornell.edu>
2016-09-26 11:28:06 -0700

    Reorganizing things a little (some structure) now that we have a few of these & things are moving forward quickly.

--- emodularization/utils/datetime_setup.py
+++ emodularization/utils/datetime_setup.py
@@ -6,25 +6,25 @@ os.environ['CXX'] = 'g++'
 os.environ['CPP'] = 'g++'
 #os.environ['LDSHARED'] = 'g++'
 
-module1 = Extension('dtk_malariachallenge',
+module1 = Extension('dtk_datetime',
         sources = [
-            'dtk_mciv_module.c',
-            '../interventions/MalariaChallenge.cpp'
+            'dtk_datetime_module.c',
+            '../../utils/IdmDateTime.cpp'
         ],
         extra_compile_args = ["-std=c++11", "-w", "-fpermissive" ], 
         include_dirs=[
-            '../interventions/',
-            '../campaign/',
-            '../utils/', 
-            '../cajun/include/', 
-            '../rapidjson/include/', 
-            '../Eradication/', 
+            '../../interventions/',
+            '../../campaign/',
+            '../../utils/', 
+            '../../cajun/include/', 
+            '../../rapidjson/include/', 
+            '../../Eradication/', 
             ],
         libraries = ['utils', 'campaign', 'cajun' ],
         library_dirs = [
-            '../build/x64/Release/utils/',
-            '../build/x64/Release/campaign/',
-            '../build/x64/Release/cajun/',
+            '../../build/x64/Release/utils/',
+            '../../build/x64/Release/campaign/',
+            '../../build/x64/Release/cajun/',
             ]
         )
 
--- emodularization/utils/mathfunc_setup.py
+++ emodularization/utils/mathfunc_setup.py
@@ -8,23 +8,23 @@ os.environ['CPP'] = 'g++'
 module1 = Extension('dtk_mathfunc',
         sources = [
             'dtk_mathfunc_module.c',
-            '../utils/MathFunctions.cpp',
-            '../utils/Environment.cpp'
+            '../../utils/MathFunctions.cpp',
+            '../../utils/Environment.cpp'
         ],
         extra_compile_args = ["-std=c++11", "-w", "-fpermissive" ], 
         include_dirs=[
-            '../interventions/',
-            '../campaign/',
-            '../utils/', 
-            '../cajun/include/', 
-            '../rapidjson/include/', 
-            '../Eradication/', 
+            '../../interventions/',
+            '../../campaign/',
+            '../../utils/', 
+            '../../cajun/include/', 
+            '../../rapidjson/include/', 
+            '../../Eradication/', 
             ],
         libraries = ['utils', 'campaign', 'cajun' ],
         library_dirs = [
-            '../build/x64/Release/utils/',
-            '../build/x64/Release/campaign/',
-            '../build/x64/Release/cajun/',
+            '../../build/x64/Release/utils/',
+            '../../build/x64/Release/campaign/',
+            '../../build/x64/Release/cajun/',
             ]
         )
 
