ff3be76a525e6c0242724bc0354297b80dae0e54
Jonathan Bloedow <jhb4@cornell.edu>
2016-11-07 12:50:18 -0800

    Updated code to enable testing of typhoid vaccine (not using framework at this time).

--- emodularization/interventions/tyvac_iv_test.py
+++ emodularization/interventions/tyvac_iv_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import json
 import sys
 sys.path.append( "./build/lib.linux-x86_64-2.7" )
 import dtk_tyvac as tyvac
@@ -13,11 +14,52 @@ def get_schema():
     schema = tyvac.get_schema()
     print( schema )
 
-# individual stuff
-tyvac.distribute()
+def test_contact_shedding():
+    tv_json = json.loads( open( "tv.json" ).read() )
+    tv_json[ "Mode" ] = "Shedding"
+    with open( "tv.json", "w" ) as tv_writer:
+        tv_writer.write( json.dumps( tv_json, indent=4, sort_keys = True ) )
 
-# Update not working yet. Crashes during callback
-tyvac.update( 1 )
+    tyvac.distribute() 
+    tyvac.update( 1 ) 
+    mods = tyvac.get_modifiers() 
+
+    contact_shed = mods[0]
+    contact_exp = mods[1]
+    contact_num = mods[2]
+    enviro_shed = mods[3]
+    enviro_exp = mods[4]
+    enviro_num = mods[5]
+
+    eff = tv_json[ "Changing_Effect" ][ "Initial_Effect" ]
+    if eff != 1 - contact_shed:
+        print( "Failed." )
+    else:
+        print( "Pass." )
+
+def test_contact_dose():
+    tv_json = json.loads( open( "tv.json" ).read() )
+    tv_json[ "Mode" ] = "Dose"
+    with open( "tv.json", "w" ) as tv_writer:
+        tv_writer.write( json.dumps( tv_json, indent=4, sort_keys = True ) )
+
+    tyvac.distribute()
+    tyvac.update( 1 )
+
+    mods = tyvac.get_modifiers() 
+    contact_shed = mods[0]
+    contact_exp = mods[1]
+    contact_num = mods[2]
+    enviro_shed = mods[3]
+    enviro_exp = mods[4]
+    enviro_num = mods[5]
+
+    eff = tv_json[ "Changing_Effect" ][ "Initial_Effect" ]
+    if eff != 1 - contact_exp:
+        print( "Failed." )
+    else:
+        print( "Pass." )
+
 
 # batch stuff
 def test_batch():
@@ -26,4 +68,6 @@ def test_batch():
     for i in range( 4000 ):
         tyvac.update_batch( 1 )
 
-test_batch()
+#test_batch()
+test_contact_shedding()
+test_contact_dose()
