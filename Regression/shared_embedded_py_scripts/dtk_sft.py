1c239c76ee7c8cf3f554c4d1dd9cab06d3f525db
YeChen-IDM <yechen@idmod.org>
2016-11-08 12:47:47 -0800

    add SFT: Dose-response_Probability
    Conflicts:
    	Regression/shared_embedded_py_scripts/dtk_sft.py

--- Regression/shared_embedded_py_scripts/dtk_sft.py
+++ Regression/shared_embedded_py_scripts/dtk_sft.py
@@ -30,13 +30,14 @@ def test_lognorm(timers,mu,sigma,report_file, category):
     else:
         s = result[0]
         p = result[1]
-    report_file.write("s is {0}, p is : {1} for {2}.\n".format(s, p, category))
+    #report_file.write("s is {0}, p is : {1} for {2}.\n".format(s, p, category))
 
     # calculate the critical values for Statistic from KS table
-    ks_table=[0.975, 0.842, 0.708, 0.624, 0.565, 0.521, 0.486, 0.457, 0.432, 0.410 , 0.391, 0.375, 0.361, 0.349, 0.338, 0.328, 0.318, 0.309, 0.301, 0.294, 0.270, 0.240, 0.230]
+    ks_table = [0.975, 0.842, 0.708, 0.624, 0.565, 0.521, 0.486, 0.457, 0.432, 0.410, 0.391, 0.375, 0.361, 0.349, 0.338,
+                0.328, 0.318, 0.309, 0.301, 0.294, 0.270, 0.240, 0.230]
     critical_value_s = 0
     if len(timers) <= 20:
-        critical_value_s = ks_table[len(timers)-1]
+        critical_value_s = ks_table[len(timers) - 1]
     elif len(timers) <= 25:
         critical_value_s = ks_table[20]
     elif len(timers) <= 30:
@@ -44,11 +45,27 @@ def test_lognorm(timers,mu,sigma,report_file, category):
     elif len(timers) <= 35:
         critical_value_s = ks_table[22]
     else:
-        critical_value_s = 1.36/math.sqrt(len(timers))
+        critical_value_s = 1.36 / math.sqrt(len(timers))
 
     if s > critical_value_s or p < 5e-2:
-        report_file.write("BAD: log normal kstest result for {0} is: statistic={1}, pvalue={2}, expected s close to {3} and p larger than 0.05.\n".format(category, s, p, critical_value_s)) 
+        report_file.write("BAD: log normal kstest result for {0} is: statistic={1}, pvalue={2}, expected s less than {3} and p larger than 0.05.\n".format(category, s, p, critical_value_s))
         return False
     else:
         return True
-    
+
+def test_poisson (trails, rate,report_file, route):
+    """
+        kstest for poisson distribution has a bug so I am testing it based on probability of events for a Possion distribution
+    """
+    #calculate probability of k events for a Possion distribution
+    success= True
+    for k in trails:
+        a = math.exp(-1.0*rate)
+        b = math.pow(rate,float(k))
+        c = math.factorial(int(k))
+        p = a * b / float(c)
+        #report_file.write("Poisson probability for {0} at rate {1} , # of event {2} is {3}, expected larger than 0.05.\n".format(route, rate, k, p))
+        if p*len(trails) < 1:
+            success = False
+            report_file.write("BAD: Poisson probability for {0} at rate {1} , # of event {2} is {3}, expected larger than {4}.\n".format(route, rate, k, p, 1/float(len(trails))))
+    return success
