eaf4a320f3fdee09a1e9dbb9164a06244658a17b
YeChen-IDM <yechen@idmod.org>
2016-11-09 13:53:28 -0800

    update to use func in dtk_sft.py

--- Regression/Typhoid/SFTs/Pre_to_Acu_Transition_Probability/dtk_post_process.py
+++ Regression/Typhoid/SFTs/Pre_to_Acu_Transition_Probability/dtk_post_process.py
@@ -3,6 +3,8 @@
 # All infections begin in prepatent.
 # The proportion of individuals who move to acute infections is determined by the config parameter Config:TSF. The remainder shall move to subclinical.
 # All new acute cases and subclinical cases are transited from prepatent state only.
+# This test passes when the number of transimission from prepatent to Acute cases is within Binormal 95% confidence interval
+# for each test, there is 5% of chance that we will reject the hypothesis while it's true
 
 import re
 import json
@@ -11,7 +13,7 @@ import pdb
 import os
 import dtk_sft as sft
 
-# C version: infectiousness = exp( -1 * _infectiousness_param_1 * pow(duration - _infectiousness_param_2,2) ) / _infectiousness_param_3;
+
 def get_val( key, line ):
     regex = key + "(\d*\.*\d*)"
     match = re.search(regex, line)
@@ -80,10 +82,16 @@ def application( report_file ):
                 success = False
                 report_file.write("Found no individual exits Prepatent state in log.\n")
             else:
-                actual_tsf=acutecount/float(subcount+acutecount)
-                if math.fabs( actual_tsf - tsf)>5e-2 :
+                # for cases that binormal confidence interval will not work
+                if tsf < 1e-2 or tsf >0.99:
+                    actual_tsf=acutecount/float(subcount+acutecount)
+                    if math.fabs( actual_tsf - tsf)>5e-2 :
+                        success = False
+                        report_file.write("BAD: Proportion of prepatent cases that become acute vs. subclinical is {0} instead of {1}. Actual Acute case = {2} vs. Actual SubClinical case = {3}.\n".format(actual_tsf,tsf, acutecount, subcount))
+                #use binormal 95% confidence interval
+                elif not sft.test_binormal_95ci(acutecount, subcount+acutecount, tsf ,report_file, 'PrePatent to Acute transmission'):
                     success = False
-                    report_file.write("BAD: Proportion of prepatent cases that become acute vs. subclinical is {0} instead of {1}. Actual Acute case = {2} vs. Actual SubClinical case = {3}.\n".format(actual_tsf,tsf, acutecount, subcount))
+
 
         if success:
             report_file.write( sft.format_success_msg( success ) )
